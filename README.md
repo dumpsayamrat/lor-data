6c26f3eb053074eed150043a18129a470786c854
dumpsayamrat <sayamrat.kt@gmail.com>
2022-07-24 23:40:05 +0700

    Add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# My data engineer project
+This is a repo where I practice and try to learn tech stack for data engineering
+
+## What have I learned so far?
+Data exploration: [Selenuim](https://www.selenium.dev/)
+Data processing: [Parquet](https://parquet.apache.org/) and [Spark](https://spark.apache.org/)
+Data orchestration platform: [Dagster](https://dagster.io/)
+
+
+## What's next?
+- Using Delta Lake for adding Database features to cloud storage
+- Build pipelines to loading data from cloud storage to store in data warehouse for analytic purposes
+- Data visualization using Apache superset
+- ML
--- pipelines/pipelines/ops/lor_scraping.py
+++ pipelines/pipelines/ops/lor_scraping.py
@@ -2,17 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 from dagster import op
-import pyspark
-from delta import *
-import dagster_pyspark
-from delta.tables import *
 from pyspark.sql.functions import *
-import os
-from pyspark.sql import DataFrame, Row, SparkSession
+from pyspark.sql import DataFrame, Row
 from pyspark.sql.types import IntegerType, StringType, StructField, StructType, ArrayType
+import time
+
+def wait_until_specific_condition(func, timeout=30):
+  endtime = time.time() + timeout
+  while True:
+      if time.time() > endtime:
+          raise TimeoutException("The condition is not satisfied")
+      result = func()
+      if result is True:
+          return result
 
 def find_champion_names(selector, parent):
   champ_names = parent.find_elements(By.XPATH, ".//div[@class='{}']".format(selector))
@@ -30,10 +37,10 @@ def gen_deck_champions(champion_names):
     })
   return champions
 
-def find_deck_againts(selector, stat_element):
-  best_deck_container = stat_element.find_element(By.CLASS_NAME, selector)
-  best_deck_elements = stat_element.find_element(By.CLASS_NAME, "gridtwostat") \
-    .find_elements(By.XPATH, ".//div[@class='bestcolorstats metacontent metacontentbest ng-star-inserted']")
+def find_deck_againts(selectors, stat_element):
+  best_deck_container = stat_element.find_element(By.CLASS_NAME, selectors[0])
+  best_deck_elements = best_deck_container.find_element(By.CLASS_NAME, "gridtwostat") \
+    .find_elements(By.XPATH, ".//div[contains(@class, 'metacontent')]")
   rows = []
   for deck in best_deck_elements:
     champion_names = find_champion_names("champname best", deck)
@@ -41,7 +48,7 @@ def find_deck_againts(selector, stat_element):
     champions = gen_deck_champions(champion_names)
     play_info = deck.find_elements(By.CLASS_NAME, "playrate")
     info_text_list = list(map(lambda n: n.text, play_info))
-    matches = info_text_list[0].split("\n")[1]
+    matches = int(info_text_list[0].split("\n")[1])
     win_rate = info_text_list[1].split("\n")[1]
     rows.append(Row(deck_name=deck_name, champions=champions, win_rate=win_rate, matches=matches))
   return rows
@@ -51,23 +58,21 @@ def find_deck_againts(selector, stat_element):
   required_resource_keys={"pyspark", "pyspark_step_launcher"}
 )
 def get_lor_win_rate(context) -> DataFrame:
-
-  # creates master dataframe
-  # df = pd.DataFrame(columns=["Player", "Salary", "Year"])
   champion_schema = ArrayType(StructType([StructField("champ_name", StringType()), StructField("region", StringType())]))
   matches_meta_schema = ArrayType(StructType([
     StructField("deck_name", StringType()),
     StructField("champions", champion_schema),
     StructField("win_rate", StringType()),
-    StructField("matches", StringType()),
+    StructField("matches", IntegerType()),
   ]))
   schema = StructType([
     StructField("deck_name", StringType()),
     StructField("champions", champion_schema),
-    StructField("matches", StringType()),
     StructField("win_rate", StringType()),
+    StructField("matches", IntegerType()),
     StructField("play_rate", StringType()),
     StructField("matches_best", matches_meta_schema, True),
+    StructField("matches_worst", matches_meta_schema, True),
   ])
   rows = []
 
@@ -75,9 +80,16 @@ def get_lor_win_rate(context) -> DataFrame:
 
   url = "https://runeterra.ar/stats"
   driver.get(url)
+
+  body = driver.find_element(By.TAG_NAME, "body")
+  no_of_pagedowns = 20
+  while no_of_pagedowns:
+      body.send_keys(Keys.PAGE_DOWN)
+      time.sleep(0.5)
+      no_of_pagedowns-=1
+
   stats = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.XPATH, "//div[starts-with(@id,'stat')]"))
-  # stats = driver.find_elements(By.XPATH, "//div[starts-with(@id,"stat")]")
-  context.log.info("stats: {}".format(len(stats)))
+  
   for stat in stats:
     meta = stat.find_element(By.XPATH, ".//div[starts-with(@id,'meta')]")
     champion_names = find_champion_names("champname", meta)
@@ -86,19 +98,20 @@ def get_lor_win_rate(context) -> DataFrame:
 
     play_info = meta.find_elements(By.CLASS_NAME, "playrate")
     info_text_list = list(map(lambda n: n.text, play_info))
-    matches = info_text_list[1].split("\n")[1]
+    matches = int(info_text_list[1].split("\n")[1])
     win_rate = info_text_list[2].split("\n")[1]
     play_rate = info_text_list[3].split("\n")[1]
 
-    matches_best = find_deck_againts("metastatdetailsbest", stat)
-
-    rows.append(Row(deck_name=deck_name, champions=champions, win_rate=win_rate, matches=matches, play_rate=play_rate, matches_best=matches_best))
+    matches_best = find_deck_againts(("metastatdetailsbest", "ng-star-inserted"), stat)
+    matches_worst = find_deck_againts(("metastatdetailsworst", "ng-star-inserted"), stat)
+    context.log.info("DECK: {} has {} win rate and {} best matches, {} worst matches".format(deck_name, win_rate, len(matches_best), len(matches_worst))) 
+    rows.append(Row(deck_name=deck_name, champions=champions, win_rate=win_rate, matches=matches, play_rate=play_rate, matches_best=matches_best, matches_worst=matches_worst))
 
   driver.close()
   return context.resources.pyspark.spark_session.createDataFrame(rows, schema)
 
 @op(required_resource_keys={"pyspark", "pyspark_step_launcher"})
 def write_to_file(players: DataFrame) -> DataFrame:
-  # players.write.save("./temp/test-par")
+  players.write.mode("overwrite").save("./temp/lor-win-rate")
   players.toPandas().to_csv("./temp/lor-winrate.csv")
   return players
